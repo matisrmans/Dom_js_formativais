ac7fcf99d76134df968b2eb07a1bc2b5f1d7d496
Nezkas <matiss.urmanis3@gmail.com>
2025-11-14 09:35:04 +0200

    salaboju inputGuess, tagad strādāju ar history

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('#button');
 const guess = document.getElementById('guess');
-const input = document.getElementById('input');
+const input = document.getElementById('number');
+const history = document.getElementById('history');
 
 button.addEventListener('click', guessCorrect);
 
@@ -12,20 +13,25 @@ function guessCorrect(){
     console.log(randomNum);
 
     if (inputGuess > randomNum){
-        guess.textContent = "Too High";
+        guess.textContent = `too High`;
         attempts++;
     }if(inputGuess < randomNum){
-        guess.innerHTML = "Too Low";
+        guess.innerHTML = `Too Low`;
         attempts++;
     }if(inputGuess == randomNum){
         guess.innerHTML = `Congrats! You guessed the number in ${attempts} attempts`;
     }
-    else{
+    if(inputGuess > 100){
         guess.innerHTML = "input a number between 1 and 100";
     }
+
 
 }
 
 function randomGen(){
     return Math.floor(Math.random() * 100)+1;
+}
+
+function saveHistory(){
+    
 }
